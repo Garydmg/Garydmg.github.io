8450e773b359b911e87846278ceb4a106ff9f271
Garydmg <gdmut2013@gmail.com>
2018-11-02 02:23:30 -0400

    Update draw.js

--- js/draw.js
+++ js/draw.js
@@ -56,7 +56,7 @@ var margin = {
     },
 
 width = 900 - margin.left - margin.right,
-height = 350 - margin.top - margin.bottom;
+height = 300 - margin.top - margin.bottom;
 
 // chart titles
 X_AXIS_TITLE = "Time Elapsed (sec)";
